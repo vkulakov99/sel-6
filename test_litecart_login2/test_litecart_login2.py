a27b345a0a90910e1d482a656fd5b48575b9d596
Vladimir Kulakov <vkulakov@gmail.com>
2017-04-24 00:40:53 +0300

    Тест для захода в тестовое приложение litecart переделан под py.test.
    Тест для захода в тестовое приложение litecart переделан под py.test.

---
+++ test_litecart_login2/test_litecart_login2.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_litecart_login2(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_css_selector('button[name="login"]').click()
+    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
