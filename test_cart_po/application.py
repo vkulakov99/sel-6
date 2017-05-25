03f4d2a3c7c0c8b3fa4e30823cb03881eba0c284
Vladimir Kulakov <vkulakov@gmail.com>
2017-05-25 12:39:39 +0300

    Подчистил огрехи
    Подчистил огрехи

--- test_cart_po/application.py
+++ test_cart_po/application.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from pages.product_page import ProductPage
 from pages.cart_page import CartPage
 from pages.main_page import MainPage
@@ -15,25 +14,16 @@ class Application:
         self.product_page = ProductPage(self.driver)
         self.cart_page = CartPage(self.driver)
 
+
     def quit(self):
         self.driver.quit()
 
+
     def add_product(self, product_number, size_index):
-        # Сохраняем начальное число товаров в корзине
-        ititial_number_of_items = self.get_number_of_items_in_cart()
-
+        ititial_number_of_items = self.get_number_of_items_in_cart() # Сохраняем начальное число товаров в корзине
         self.product_page.open(product_number) # открываем продукт с порядковым номером на странице product_number
-
-        self.product_page.select_size(size_index)
-        #size = Select(self.driver.find_element_by_name('options[Size]'))
-        #size.select_by_index(product.size)  # нулевая опция - это просто подсказка --Select--, поэтому начинаем с 1
-
-        self.product_page.add_to_cart(ititial_number_of_items)
-        #button = self.driver.find_element_by_css_selector("button[name=add_cart_product]")
-        #button.click()
-
-        # Ждем, пока количество товаров в корзине увеличится на 1
-        #WebDriverWait(self.driver,10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart .quantity"),str(ititial_number_of_items+1)))
+        self.product_page.select_size(size_index) # выбираем размер продукта
+        self.product_page.add_to_cart(ititial_number_of_items) # добавляем товар и ждем пока объем корзины увеличится
 
 
     def get_number_of_items_in_cart(self):
--- test_cart_po/test_cart_po.py
+++ test_cart_po/test_cart_po.py
@@ -1,5 +1,4 @@
 import pytest
-from selenium import webdriver
 from application import Application
 
 
@@ -8,32 +7,15 @@ def app(request):
     app = Application()
     request.addfinalizer(app.quit)
     return app
-# def driver(request):
-#     wd = webdriver.Chrome()
-#     request.addfinalizer(wd.quit)
-#     return wd
 
 
 def test_cart_po(app):
 
-
-    # Описываем новый класс Product
-    class Product:
-        def __init__(self, size=None):
-            self.size = size
-
     for product_counter in range(0,3):
-
-        # Создаем объект класса Product
-        product = Product(size = product_counter+1)
-
         # Открываем главную страницу
         app.open_main_page()
-
         # Вызываем функцию добавления продукта
         app.add_product(0, product_counter) # нулевой продукт в списке продуктов, размеры нумеруются с нуля
-
-
 
     # Удаляем товары из корзины по очереди
     for product_counter in range(0,3):
